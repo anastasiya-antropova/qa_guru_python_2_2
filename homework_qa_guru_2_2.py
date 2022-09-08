ed7ff7c840d73359f5cfeecb31985dd2223d2dc2
anastasiya.a.antropova <antropova.anastasiya.a@gmail.com>
2022-09-08 16:25:40 +0200

    Обновлена домашняя работа по занятию 2_2

--- homework_qa_guru_2_2.py
+++ homework_qa_guru_2_2.py
@@ -2,13 +2,13 @@ from selene import be, have
 from selene.support.shared import browser
 
 # Открытие результатов по строке поиска
-def test_find(size_window, open_browser):
+def test_find(open_browser):
     browser.element('[name=q]').should(be.blank).type('selene').press_enter()
 
-# Поиск нужного результата среди всего пула
-def test_open_page():
+# Позитивный тест
+def test_positive():
     browser.element('[id=search]').should(have.text('User-oriented Web UI browser tests in Python'))
 
-# Провальный тест
-def test_open_yandex(after_end):
-    browser.element('[button=yandex]').press_enter()
+# Негативный тест
+def test_negative(after_end):
+    browser.element('[id=search]').should_not(have.text('ya Chebur'))
